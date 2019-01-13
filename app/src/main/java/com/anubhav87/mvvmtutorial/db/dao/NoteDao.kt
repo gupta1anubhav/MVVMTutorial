a0e3b9374a41788cf7235184ffad807820e0e627
gupta1anubhav <anubhav.ankit87@gmail.com>
2019-01-13 15:46:49 +0530

    Dao added

---
+++ app/src/main/java/com/anubhav87/mvvmtutorial/db/dao/NoteDao.kt
@@ -0,0 +1,25 @@
+package com.anubhav87.mvvmtutorial.db.dao
+
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.*
+import com.anubhav87.mvvmtutorial.db.entity.Note
+
+@Dao
+interface NoteDao {
+
+    @Insert
+    fun insert(note: Note)
+
+    @Update
+    fun update(note: Note)
+
+    @Delete
+    fun delete(note: Note)
+
+    @Query("DELETE FROM notes_table")
+    fun deleteAllNotes()
+
+    @Query("SELECT * FROM notes_table ")
+    fun getAllNotes(): LiveData<List<Note>>
+
+}
