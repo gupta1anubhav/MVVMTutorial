2fb525f0e136e0a370d430b27f9e89fd9ddcdafc
gupta1anubhav <anubhav.ankit87@gmail.com>
2019-01-13 16:58:24 +0530

    ViewModel  added

---
+++ app/src/main/java/com/anubhav87/mvvmtutorial/viewmodel/NoteViewModel.kt
@@ -0,0 +1,27 @@
+package com.anubhav87.mvvmtutorial.viewmodel
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.LiveData
+import com.anubhav87.mvvmtutorial.NoteRepository
+import com.anubhav87.mvvmtutorial.db.entity.Note
+
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var repository: NoteRepository =
+        NoteRepository(application)
+    private var allNotes: LiveData<List<Note>> = repository.getAllNotes()
+
+    fun insert(note: Note) {
+        repository.insert(note)
+    }
+
+    fun deleteAllNotes() {
+        repository.deleteAllNotes()
+    }
+
+    fun getAllNotes(): LiveData<List<Note>> {
+        return allNotes
+    }
+}
