989ed64767c1a9ebd038438779800388b4bb5b9d
gupta1anubhav <anubhav.ankit87@gmail.com>
2019-01-13 15:52:44 +0530

    Databse  added

---
+++ app/src/main/java/com/anubhav87/mvvmtutorial/db/NoteDatabase.kt
@@ -0,0 +1,36 @@
+package com.anubhav87.mvvmtutorial.db
+
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import com.anubhav87.mvvmtutorial.db.dao.NoteDao
+import com.anubhav87.mvvmtutorial.db.entity.Note
+
+@Database(entities = [Note::class], version = 1)
+abstract class NoteDatabase : RoomDatabase() {
+
+    abstract fun noteDao(): NoteDao
+
+
+    companion object {
+        private var instance: NoteDatabase? = null
+
+        fun getInstance(context: Context): NoteDatabase? {
+            if (instance == null) {
+                synchronized(NoteDatabase::class) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        NoteDatabase::class.java, "note_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                }
+            }
+            return instance
+        }
+
+    }
+
+}
